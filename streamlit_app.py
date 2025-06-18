a1d08d0bb2ec54b5da9662539f08b39543161540
ss-v44 <venkatesh@specsavers.co.za>
2025-06-18 19:57:52 +0200

    Update streamlit_app.py
    Requests imported

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,13 +19,11 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 
-
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients",
     my_dataframe,
     max_selections = 5
 )
-
 
 
 if ingredients_list:
@@ -46,3 +44,6 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
 
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
